cb33b14c5fd510cabbb85770041e5d66d4477ab3
Kotya Shmotoloha <kostiashmotoloha@gmaol.com>
2019-11-21 19:42:44 +0200

    Updates

---
+++ precache-manifest.d06abc7cca6c0895a33a54ef04f92254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0be6129a2aa15c91ba25a3dd8a44b9a",
+    "url": "/reader/index.html"
+  },
+  {
+    "revision": "914cfe22c0a0fb498487",
+    "url": "/reader/static/css/main.752231ba.chunk.css"
+  },
+  {
+    "revision": "46856ea4fde790fc9437",
+    "url": "/reader/static/js/2.05f4d8f9.chunk.js"
+  },
+  {
+    "revision": "914cfe22c0a0fb498487",
+    "url": "/reader/static/js/main.47a30bd6.chunk.js"
+  },
+  {
+    "revision": "34196869e8d1ea625a0a",
+    "url": "/reader/static/js/runtime-main.7ea57bde.js"
+  }
+]);
